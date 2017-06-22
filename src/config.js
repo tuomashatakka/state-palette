f807dc1d893da5787adf4821bc482a48d66f448c
Tuomas Hatakka <tuomas.hatakka@fns.fi>
2017-06-22 05:19:26 +0300

    Settings interface for colors & select icons by providing path

---
+++ src/config.js
@@ -0,0 +1,79 @@
+'use babel'
+
+import filesystem from 'fs'
+
+function resolveValue (val) {
+  if (!val)
+    return ''
+  if (val.toJSON)
+    return val.toJSON()
+  if (val.toJS)
+    return val.toJS()
+  if (val.toString)
+    return val.toString()
+  return val
+}
+
+function prefix (key) {
+  let space
+  key.unshift('stateful')
+  key = key.join('-')
+  space = Array(30-key.length).join(' ')
+  return `@${key}:${space}`
+}
+
+function applyCss (path) {
+  let { themes } = atom
+  let src = themes.loadLessStylesheet(path)
+  themes.applyStylesheet(path, src, 5)
+  themes.refreshLessCache()
+  return src
+}
+
+export function writeLessVariable (fp, config={}) {
+
+  console.warn("ARGUMENTS PASSED TO writeLessVariable", arguments)
+
+  const print = (val, ...key) => `${prefix(key)}${resolveValue(val)};`
+  const iterate = cat => Object
+    .keys(config[cat] || {})
+    .map(key => print(config[cat][key], cat, key))
+    .join('\n')
+  let stream = [
+    iterate('icon'),
+    iterate('color'),
+  ].join('\n')
+
+  console.info(`Writing the less config
+    to path ${fp}
+    ----------------
+    ${stream}`)
+  try {
+    filesystem.writeFile(fp, stream + '\n', 'utf8', err => err ?
+      error(err, "Writing less variables to a file failed") :
+      applyCss(fp))
+  }
+  catch(e) {
+    error(e, "Writing less variable failed") }
+
+}
+
+export function error (e, ...msg) {
+
+
+  let { name } = require('../package.json')
+  let title = `${name} Package Error`
+  let description = msg.reduce((str, message) => str + `<p>${message}</p>`, `<h4>${e.message}</h4>`)
+  let buttons = {
+    'Open developer tools': () => atom.openDevTools() }
+
+  if (atom.devMode)
+    // eslint-disable-next-line
+    console.warn(`${name}: ${e}`)
+
+  atom.notifications.addError(title, {
+    description,
+    dismissable: true,
+    buttons: Object.keys(buttons).map(text => ({ text, onDidClick: () => buttons[text]() }))
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 'use babel'
 
+import { CompositeDisposable } from 'atom'
+import { writeLessVariable } from './config'
+import { join } from 'path'
+
+const config = require('./config.json')
+const pack = require('../package.json')
+let subscriptions
 
 export default {
+  config,
   initialize: () => {
-    this.opener = atom.workspace.addOpener(url => url.startsWith('state:') ? document.createElement('div') : null)
-    console.info(this.opener, this)
+    subscriptions = new CompositeDisposable()
+    subscriptions.add(
+      atom.workspace.addOpener(url => url.startsWith('state:') ? document.createElement('div') : null),
+    )
   },
-  activate: () => {},
+  activate: () => {
+    console.info(this)
+    let { path } = atom.packages.getLoadedPackage(pack.name)
+    let fp = join(path, 'styles', 'conf.less')
+    let fn = conf => writeLessVariable(fp, conf)
+    let configChange = atom.config.observe(pack.name, fn)
+    console.info(path, fp, fn, configChange)
+    subscriptions.add(configChange)
+  },
   deactivate: () => {
-    this.opener.dispose()
+    subscriptions.dispose()
   },
 }
